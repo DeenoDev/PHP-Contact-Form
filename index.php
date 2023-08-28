e5a1426de5a6306608337ff8a9feda20ddef5633
DeenoDev <deyanoklaasen@outlook.com>
2023-08-28 19:45:41 +0200

    HTML for contact form completed.

--- index.php
+++ index.php
@@ -7,6 +7,17 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <main>
+        <p>SEND E-Mail</p>
+        <form class="contact-form" action="contactform.php" method="post">
+            <input type="text" name="name" placeholder="Full Name">
+            <input type="text" name="mail" placeholder="Your E-mail">
+            <input type="text" name="subject" placeholder="Subject">
+            <textarea name="message" placeholder="Message"></textarea>
+            <button type="submit" name="submit">SEND MAIL</button>
+
+        </form>
+    </main>
     
 </body>
 </html>
